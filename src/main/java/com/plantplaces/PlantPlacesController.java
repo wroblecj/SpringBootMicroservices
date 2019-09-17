849b833a8e2f7a2c0f5cc25ca7b35b048e75ed59
Administrator <Administrator@DESKTOP-099UQRG>
2019-09-17 14:39:34 -0400

    test

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -3,6 +3,7 @@ package com.plantplaces;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+//Test
 @Controller
 public class PlantPlacesController {
 
